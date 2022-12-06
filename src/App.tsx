ac006f3c6726baccb19baac8900765839e503290
Steven4565 <stevenchristian4565@gmail.com>
2022-12-06 21:28:53 +0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [url, setUrl] = useState<string>('');
+
+  useEffect(() => {
+    const queryInfo = { active: true, lastFocusedWindow: true };
+
+    chrome.tabs &&
+      chrome.tabs.query(queryInfo, (tabs) => {
+        if (!tabs[0].url) return;
+
+        const url = tabs[0].url;
+        setUrl(url);
+      });
+  }, []);
+
+  return <div className="App">
+    {url}
+  </div>;
 }
 
 export default App;
